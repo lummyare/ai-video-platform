b32f6a5d95b716d5d219c7814c4a216994cdc4d7
johnson are <lummyare@gmail.com>
2025-05-03 19:42:37 -0500

    Create index.tsx

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+import TextToVideoGenerator from '../components/TextToVideoGenerator';
+
+export default function Home() {
+  return (
+    <Layout>
+      <TextToVideoGenerator />
+    </Layout>
+  );
+}
