2a919b5258519d456adaa5105b2addc436bf4518
johnson are <lummyare@gmail.com>
2025-05-03 22:27:09 -0500

    Update TextToVideoGenerator.tsx

--- frontend/src/components/TextToVideoGenerator.tsx
+++ frontend/src/components/TextToVideoGenerator.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import { api } from '@/lib/api';
+import { api } from '../lib/api';
 
 interface GenerationForm {
   prompt: string;
